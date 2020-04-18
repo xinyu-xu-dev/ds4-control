e90996904c587ede476a338e884f765e18da49aa
xinyu-xu-dev <xinyu.xu.life@hotmail.com>
2020-04-18 16:28:40 +0100

    added check-config and parser

---
+++ scripts/ds4-control-check-config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import time
+import serial
+
+serial_comm = serial.Serial(
+    port = '/dev/ttyAMA0',
+#    baudrate = 9600,
+    baudrate = 115200,
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE,
+    bytesize = serial.EIGHTBITS,
+    timeout = 1
+)
+
+serial_comm.write('?\r'.encode())
+
+while 1:
+
+    received_message = serial_comm.readline()
+    print(received_message)
