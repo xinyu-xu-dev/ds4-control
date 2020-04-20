f7de2594cd0f28d54a3b0a14321f61f3e8833aab
xinyu-xu-dev <xinyu.xu.life@hotmail.com>
2020-04-20 23:34:50 +0100

    corrected i2c comm function

--- scripts/ds4_i2c_control.py
+++ scripts/ds4_i2c_control.py
@@ -50,47 +50,47 @@ dic_ds4_properies = {
     }
 
 def get_joystick_left_X():
-    val = bus.read_byte_data(0, 1)
+    val = bus.read_byte_data(i2c_address, 0)
     print('Left Joystick X: ', val)
     return val
 def get_joystick_left_Y():
-    val = bus.read_byte_data(1, 1)
+    val = bus.read_byte_data(i2c_address, 1)
     print('Left Joystick Y: ', val)
     return val
 def get_joystick_right_X():
-    val = bus.read_byte_data(2, 1)
+    val = bus.read_byte_data(i2c_address, 2)
     print('Right Joystick X: ', val)
     return val
 def get_joystick_right_Y():
-    val = bus.read_byte_data(3, 1)
+    val = bus.read_byte_data(i2c_address, 3)
     print('Right Joystick Y: ', val)
     return val
 def get_accelerometer_X():
-    val = bus.read_byte_data(4, 1)
+    val = bus.read_byte_data(i2c_address, 4)
     print('Accelerometer X: ', val)
     return val
 def get_accelerometer_Y():
-    val = bus.read_byte_data(5, 1)
+    val = bus.read_byte_data(i2c_address, 5)
     print('Accelerometer Y: ', val)
     return val
 def get_L2():
-    val = bus.read_byte_data(6, 1)
+    val = bus.read_byte_data(i2c_address, 6)
     print('L2: ', val)
     return val
 def get_R2():
-    val = bus.read_byte_data(7, 1)
+    val = bus.read_byte_data(i2c_address, 7)
     print('R2: ', val)
     return val
 def get_touchpad_X():
-    val = bus.read_byte_data(11, 1)
+    val = bus.read_byte_data(i2c_address, 11)
     print('Touchpad X: ', val)
     return val
 def get_touchpad_Y():
-    val = bus.read_byte_data(12, 1)
+    val = bus.read_byte_data(i2c_address, 12)
     print('Touchpad Y: ', val)
     return val
 def get_battery_level():
-    val = bus.read_byte_data(13, 1)
+    val = bus.read_byte_data(i2c_address, 13)
     print('Battery Level: ', val)
     return val
 
